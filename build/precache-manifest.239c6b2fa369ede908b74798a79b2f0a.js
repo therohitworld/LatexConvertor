a603b76629f765bf6ea41b04098be99da426db52
therohitworld <63524953+therohitworld@users.noreply.github.com>
2020-04-28 01:26:42 +0530

    uploaded deployable build folder

---
+++ build/precache-manifest.239c6b2fa369ede908b74798a79b2f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09757e3c59488561cf4dc04628ade1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c3a1fa8f6ff6e320613",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e704acf0f5f55d5f5da4",
+    "url": "/static/js/2.63ce7c05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.63ce7c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3a1fa8f6ff6e320613",
+    "url": "/static/js/main.90fc99cc.chunk.js"
+  },
+  {
+    "revision": "3798c1fca99487eeb9ea",
+    "url": "/static/js/runtime-main.60d7c4f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
